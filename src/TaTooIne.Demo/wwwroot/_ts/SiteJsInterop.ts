4cc950d4eae2e939ea5bfe91cd5a4d049358b67d
Anatoliy Siryi <hmG3@users.noreply.github.com>
2020-08-21 11:28:09 +0300

    Use Talib version from NuGet

--- src/TaTooIne.Demo/wwwroot/_ts/SiteJsInterop.ts
+++ src/TaTooIne.Demo/wwwroot/_ts/SiteJsInterop.ts
@@ -1,6 +1,6 @@
 ﻿import type {
-    BusinessDay, ChartOptions, CrosshairMode, DeepPartial, IChartApi, ISeriesApi, LineStyle, LineType, PriceLineSource, PriceScaleMode,
-    TickMarkType, Time, UTCTimestamp
+    BarData, BusinessDay, ChartOptions, CrosshairMode, DeepPartial, HistogramData, IChartApi, ISeriesApi, LineData, LineStyle, LineType,
+    PriceLineSource, PriceScaleMode, TickMarkType, Time, UTCTimestamp
 } from './lib/lightweight-charts/dist/typings';
 
 class SiteJsInterop {
@@ -54,18 +54,19 @@ class SiteJsInterop {
                 }
             });
 
-            let resizeTimerId: number;
-            document.body.onresize = () => {
-                if (resizeTimerId) {
-                    clearTimeout(resizeTimerId);
-                }
-                resizeTimerId = setTimeout(() => this.resizeChart(), 100);
+        let resizeTimerId: number;
+        document.body.onresize = () => {
+            if (resizeTimerId) {
+                clearTimeout(resizeTimerId);
             }
+            resizeTimerId = setTimeout(() => this.resizeChart(), 100);
+        }
 
         this.chartSeriesMap = new Map<string, ISeriesApi<'Candlestick' | 'Histogram' | 'Line' | 'Bar' | 'Area'>>();
     }
 
-    setChartData(title: string, { candle, overlayLines, volume, valueLines }) {
+    setChartData(title: string, data:
+        { candle: Array<BarData>, overlayLines: Array<Array<LineData>>, volume: Array<HistogramData>, valueLines: Array<Array<LineData>> }) {
         this.chartSeriesMap.forEach((v, k, m) => {
             this.chart.removeSeries(v);
             m.delete(k);
@@ -77,16 +78,16 @@ class SiteJsInterop {
             }
         });
 
-        if (candle && Array.isArray(candle)) {
+        if (data.candle && Array.isArray(data.candle)) {
             const candleSeries = this.chart.addCandlestickSeries({
                 priceScaleId: 'left',
                 title: 'AAPL'
             });
-            candleSeries.setData(candle);
+            candleSeries.setData(data.candle);
             this.chartSeriesMap.set('candle', candleSeries);
         }
 
-        if (volume && volume.length) {
+        if (data.volume && data.volume.length) {
             const volumeSeries = this.chart.addHistogramSeries({
                 priceFormat: {
                     type: 'volume'
@@ -99,23 +100,23 @@ class SiteJsInterop {
                     bottom: 0
                 }
             });
-            volumeSeries.setData(volume);
+            volumeSeries.setData(data.volume);
             this.chartSeriesMap.set('volume', volumeSeries);
         }
 
-        if (overlayLines && Array.isArray(overlayLines)) {
-            for (let i = 0; i < overlayLines.length; i++) {
+        if (data.overlayLines && Array.isArray(data.overlayLines)) {
+            for (let i = 0; i < data.overlayLines.length; i++) {
                 const overlayLineSeries = this.chart.addLineSeries({
                     priceScaleId: 'right',
                     lineWidth: 1
                 });
-                overlayLineSeries.setData(overlayLines[i]);
+                overlayLineSeries.setData(data.overlayLines[i]);
                 this.chartSeriesMap.set(`overlayLine${i}`, overlayLineSeries);
             }
         }
 
-        if (valueLines && Array.isArray(valueLines)) {
-            for (let i = 0; i < valueLines.length; i++) {
+        if (data.valueLines && Array.isArray(data.valueLines)) {
+            for (let i = 0; i < data.valueLines.length; i++) {
                 const valueLineSeries = this.chart.addLineSeries({
                     priceScaleId: '',
                     scaleMargins: {
@@ -124,7 +125,7 @@ class SiteJsInterop {
                     },
                     lineWidth: 1
                 });
-                valueLineSeries.setData(valueLines[i]);
+                valueLineSeries.setData(data.valueLines[i]);
                 this.chartSeriesMap.set(`valueLine${i}`, valueLineSeries);
             }
         }
